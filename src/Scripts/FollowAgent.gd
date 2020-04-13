fdfd6da02171511b8c0bb713b468595a07bfa701
Mick McCarthy <mick@mccarthy.tech>
2020-04-13 21:23:55 +0100

    Create agent follow behaviour

--- src/Scripts/FollowAgent.gd
+++ src/Scripts/FollowAgent.gd
@@ -1,24 +1,38 @@
 extends KinematicBody2D
 
-const DISTANCE_THRESHOLD: float = 3.0
+const ARRIVE_THRESHOLD: float = 3.0
 
 onready var sprite: Sprite = $TriangleRed
+onready var target: Node = get_node(target_path)
 
+export var target_path: NodePath = NodePath()
+export var follow_offset: float = 100.0
 export var max_speed: float = 500.0
 
 var velocity: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
-	var target_global_positon: Vector2 = get_global_mouse_position()
+	if target == self:
+		set_physics_process(false)
+
+	var target_global_positon: Vector2 = target.global_position
+	var to_target: float = global_position.distance_to(target_global_positon)
 	
-	if global_position.distance_to(target_global_positon) < DISTANCE_THRESHOLD:
+	
+	if to_target < ARRIVE_THRESHOLD:
 		return
-		
-	velocity = Steering.follow(
+
+	var follow_global_position: Vector2 = (target_global_positon - 	(target_global_positon - global_position).normalized() * follow_offset
+		if to_target > follow_offset
+		else global_position
+	)
+
+	velocity = Steering.arrive_to(
 		velocity,
 		global_position,
-		target_global_positon,
-		max_speed
+		follow_global_position,
+		max_speed,
+		200.0
 	)
 	velocity = move_and_slide(velocity)
 	sprite.rotation = velocity.angle()
